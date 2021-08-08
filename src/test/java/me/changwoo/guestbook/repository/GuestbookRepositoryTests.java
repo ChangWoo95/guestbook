05fbe32b7e6bca26e2e02cfaeedc1b47ad120483
qaqa313a <qaqa313a@naver.com>
2021-08-08 23:21:52 +0900

    Test: Update test & Optional ifPresent 적용

--- src/test/java/me/changwoo/guestbook/repository/GuestbookRepositoryTests.java
+++ src/test/java/me/changwoo/guestbook/repository/GuestbookRepositoryTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -26,6 +27,28 @@ public class GuestbookRepositoryTests {
 
             System.out.println(guestbookRepository.save(guestbook));
         });
+    }
+
+    @Test
+    public void updateTest() {
+        Optional<Guestbook> result = guestbookRepository.findById(300L);
+
+        // Optional ifPresent 적용
+        result.ifPresent(rs -> {
+            rs.changeTitle("Changed Title....@@@");
+            rs.changeContent("Changed Content....!!!");
+            guestbookRepository.save(rs);
+        });
+        
+        // 기존 Test 코드
+//        if(result.isPresent()) {
+//            Guestbook guestbook = result.get();
+//
+//            guestbook.changeTitle("Changed Title....");
+//            guestbook.changeContent("Changed Content....");
+//
+//            guestbookRepository.save(guestbook);
+//        }
     }
 
 }
